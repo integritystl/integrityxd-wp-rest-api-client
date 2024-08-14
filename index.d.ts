cf0816a9f8c7e6ff839fbbf629b385b3b6f01f6e
Brian Kennedy <brian.kennedy@integrityxd.com>
2024-08-14 13:39:09 -0500

    Make declaration and implementation use generics for more flexibility

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,13 @@
 import { AxiosInstance, AxiosResponse } from 'axios';
 import { Buffer } from 'buffer';
 
-export interface WordPressItem {
-  [key: string]: unknown;
-}
-
 export interface WordPressAPI {
-  insertItem(url: string, data: WordPressItem): Promise<AxiosResponse<WordPressItem>>;
+  insertItem<T = any>(url: string, data: T): Promise<AxiosResponse<T>>;
   deleteItem(url: string): Promise<AxiosResponse<{ deleted: boolean }>>;
   forceDeleteItem(url: string): Promise<AxiosResponse<{ deleted: boolean }>>;
-  updateItem(url: string, data: WordPressItem): Promise<AxiosResponse<WordPressItem>>;
-  updateItemBuffer(url: string, data: Buffer, filename: string): Promise<AxiosResponse<WordPressItem>>;
-  fetchAllItems(url: string, extraParams?: Record<string, unknown>): Promise<WordPressItem[]>;
+  updateItem<T = any>(url: string, data: Partial<T>): Promise<AxiosResponse<T>>;
+  updateItemBuffer<T = any>(url: string, data: Buffer, filename: string): Promise<AxiosResponse<T>>;
+  fetchAllItems<T = any>(url: string, extraParams?: Record<string, unknown>): Promise<T[]>;
 }
 
 export function createWordPressAPI(axiosInstance: AxiosInstance): WordPressAPI;
