74511bf8e7e7860a6a1bf8a7660a3f4ce900ad67
Brian Kennedy <brian.kennedy@integrityxd.com>
2024-08-14 14:18:22 -0500

    Support generalized AxiosInstance and type DeleteResponse better

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,23 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
 import { Buffer } from 'buffer';
+import { DeleteResponse, AxiosLike } from './types';
 
-export function createWordPressAPI(axiosInstance: AxiosInstance) {
+export function createWordPressAPI(axiosInstance: AxiosLike) {
   return {
     async insertItem<T = any>(url: string, data: T): Promise<AxiosResponse<T>> {
-      return axiosInstance({
-        method: 'POST',
-        url,
-        data,
-      });
+      return axiosInstance.post(url, data);
     },
 
-    async deleteItem(url: string): Promise<AxiosResponse<{ deleted: boolean }>> {
-      return axiosInstance({
-        method: 'DELETE',
-        url,
-      });
+    async deleteItem<T = any>(url: string): Promise<AxiosResponse<DeleteResponse<T>>> {
+      return axiosInstance.delete(url);
     },
 
-    async forceDeleteItem(url: string): Promise<AxiosResponse<{ deleted: boolean }>> {
-      return axiosInstance({
-        method: 'DELETE',
-        url,
-        params: {
-          force: true,
-        },
-      });
+    async forceDeleteItem<T = any>(url: string): Promise<AxiosResponse<DeleteResponse<T>>> {
+      return axiosInstance.delete(url, { params: { force: true } });
     },
 
     async updateItem<T = any>(url: string, data: Partial<T>): Promise<AxiosResponse<T>> {
-      return axiosInstance({
-        method: 'PUT',
-        url,
-        data,
-      });
+      return axiosInstance.put(url, data);
     },
 
     async updateItemBuffer<T = any>(url: string, data: Buffer, filename: string): Promise<AxiosResponse<T>> {
@@ -51,14 +35,11 @@ export function createWordPressAPI(axiosInstance: AxiosInstance) {
       };
       const contentType = contentTypeMap[fileExtension.toLowerCase()] || 'application/octet-stream';
 
-      return axiosInstance({
-        method: 'POST',
-        url,
+      return axiosInstance.post(url, data, {
         headers: {
           'Content-Type': contentType,
           'Content-Disposition': `attachment; filename=${filename}`,
         },
-        data,
       });
     },
 
@@ -68,7 +49,7 @@ export function createWordPressAPI(axiosInstance: AxiosInstance) {
       let totalPages = 1;
 
       while (currentPage <= totalPages) {
-        const response = await axiosInstance<T[]>(url, {
+        const response = await axiosInstance.get<T[]>(url, {
           params: {
             page: currentPage,
             per_page: 100,
